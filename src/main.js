37f2ceaa2c9bbc303bfbe859dd2bef6d0e3e62d5
Gil <dev.javiergil@gmail.com>
2020-11-07 19:37:09 -0500

    ajuste

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import "vuetify/dist/vuetify.min.css";
+import VueRouter from 'vue-router';
 
+import Empleados from './components/Empleados';
+import Contratistas from './components/Contratistas';
+import Reporte from './components/Reporte';
+
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: '/empleados',
+      component: Empleados
+    },
+    {
+      path: '/contratistas',
+      component: Contratistas
+    },
+    {
+      path: '/reporte',
+      component: Reporte
+    }
+  ]
+})
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
